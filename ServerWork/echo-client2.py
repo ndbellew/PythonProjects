a3759c1046dc7298fb415a9f10d805644d4ec628
ndbellew <ndbellew@mavs.coloradomesa.edu>
2019-02-19 13:25:10 -0700

    created second simple echo client

--- ServerWork/echo-client2.py
+++ ServerWork/echo-client2.py
@@ -1,1 +1,16 @@
-#
+#!/usr/bin/env python3
+
+import socket
+ #local host to loopback to
+PORT = 6644 #any port above 1023 are not binded
+
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind(('', PORT))
+    s.connect(('',PORT))
+    s.sendall(b'Hello World')
+    data = s.recv(1024)
+
+
+print('Recieved', repr(data))
