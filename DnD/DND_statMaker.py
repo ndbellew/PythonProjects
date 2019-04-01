62fbed91af68a9c57f79694fa99e3f6b38255630
ndbellew <ndbellew@mavs.coloradomesa.edu>
2019-04-01 12:40:17 -0600

    created something fun

---
+++ DnD/DND_statMaker.py
@@ -0,0 +1,75 @@
+#!/bin/pyton3
+import random
+import math
+import sys
+
+class StatMaker:
+
+    def __init__(self, _STR, _DEX, _CON, _INT, _WIS, _CHA):
+        self.S = _STR
+        self.D = _DEX
+        self.C = _CON
+        self.I = _INT
+        self.W = _WIS
+        self.CH = _CHA
+        self.count = 0
+        self.GrandTotalCount = 0
+
+    def RandInt(self,Min):
+        Num = 0
+        while(Num<Min):
+            Num = random.randint(1,6)+random.randint(1,6)+random.randint(1,6)
+            self.count +=1
+        return Num
+    def SetStats(self):
+        STR=self.RandInt(self.S)
+        DEX=self.RandInt(self.D)
+        CON=self.RandInt(self.C)
+        INT=self.RandInt(self.I)
+        WIS=self.RandInt(self.W)
+        CHA=self.RandInt(self.CH)
+        return STR,DEX,CON,INT,WIS,CHA
+
+    def DetermineStats(self):
+        total = 0
+        if len(sys.argv) > 1:
+            num = int(sys.argv[1])
+        else:
+            num = 70
+        while (total<num):
+            STR,DEX,CON,INT,WIS,CHA = self.SetStats()
+            total = int(math.fsum([STR,DEX,CON,INT,WIS,CHA]))
+        return total, STR, DEX, CON, INT, WIS, CHA
+
+    def PrintStats(self,total, STR,DEX,CON,INT,WIS,CHA):
+        print("______________")
+        print("STR: ",STR,"\nDEX: ",DEX,"\nCON: ",CON,"\nINT: ",INT,"\nWIS: ",WIS,"\nCHA: ",CHA,"\nTotal: ",total)
+        print("Total Rolls to get score: ",self.count)
+        print("Grand Total Rolls this session: ", self.GrandTotalCount)
+        print("______________")
+
+    def run(self):
+        total, STR,DEX,CON,INT,WIS,CHA = self.DetermineStats()
+        self.PrintStats(total, STR,DEX,CON,INT,WIS,CHA)
+        self.GrandTotalCount += self.count
+        self.count = 0
+
+def splitvars(line):
+    for x in line.split():
+        yield int(x)
+
+def main():
+    print("HERE")
+    stats = input("Input your stat minimums in order (STR,DEX,CON,INT,WIS,CHA)\n ex. 3 3 3 3 7 3\n")
+    STR,DEX,CON,INT,WIS,CHA = splitvars(stats)
+    RollStats = StatMaker(STR,DEX,CON,INT,WIS,CHA)
+    RollAgain = True
+    while(RollAgain):
+        RollStats.run()
+        x = input("Would you like to roll again? y/n\n")
+        if x == 'n':
+            RollAgain = False
+
+
+if __name__ == "__main__":
+    main()
--- ServerWork/multiconnectionserver.py
+++ ServerWork/multiconnectionserver.py
@@ -7,6 +7,7 @@ Simple multi-connection server
 import selectors
 sel = selectors.DefaultSelector()
 import socket
+messages = [b'Message 1 from client.',b'Message 2 from client']
 
 def accept_wrapper(sock):
     conn, addr = sock.accept()
@@ -33,6 +34,14 @@ def service_connection(key, mask):
             sent = sock.send(data.outb) #Shoud be ready to write
             data.outb = data.outb[sent:]
 
+def start_connection(host, port, num_conns):
+    server_addr = (host,port)
+    for i in range(0, num_conns):
+        connid = i+1
+        print('starting connection', connid, 'to',server_addr)
+        sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        #inclomplete
+
 def main():
     HOST = "127.0.0.1" #local host to loopback to
     PORT = 6555 #any port above 1023 are not binded
@@ -42,15 +51,16 @@ def main():
     lsock.listen()
     print('listening on ', (HOST, PORT))
     lsock.setblocking(False)
-    sel.register(lsock, selectors.EVENT_READ, data=None)
+    sel.register(lsock, selectors.EVENT_READ, data=None)#registers the socket to be monitored
 
     while True:
-        events = sel.select(timeout=None)
+        events = sel.select(timeout=None)# what is being monitored. this blocks until there are sockets ready for  I/O
         for key, mask in events:
-            if key.data is None:
+            if key.data is None:# if this is true then we know its form the listening socket and we to accept()
                 accept_wrapper(key.fileobj)
             else:
                 service_connection(key,mask)
+                #if its not none then we know it's a client socket that's already been accpetd and we need to service itself.
 
 if __name__ == "__main__":
     main()
